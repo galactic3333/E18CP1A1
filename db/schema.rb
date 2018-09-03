8ab45f0d5fcba5a3b7071a8e54a192c3c2d24b05
Gabriel Alarcon <galarconesp@gmail.com>
2018-09-03 01:05:07 -0300

    commit 02: Relationship between User and Products added (1 to many)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_02_212510) do
+ActiveRecord::Schema.define(version: 2018_09_03_031423) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2018_09_02_212510) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "sold"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
